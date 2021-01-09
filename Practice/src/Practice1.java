cfd080301e4fbfdaa10679930b69ed01e8d33ab1
jangle <shubhamjangale0000@gmail.com>
2021-01-09 13:15:06 +0530

    Added : Added the furst message

---
+++ Practice/src/Practice1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Practice1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a[][] = new int[100000][100];
+	    int n , q;
+	    int k, e, f;
+	    n = sc.nextInt();
+	    for(int i= 0;i<=n;i++){
+	        k = sc.nextInt();
+	        System.out.println(k);
+	        // for(int j = 0; j < k; j++){
+	        //     cin>>a[i][j];
+	        // }
+	    }
+	  
+	}
+}
+
+
+ASDSFFGD
+// for(int i = 0; i < q; i++){
+//     cin>>e>>f;
+//     cout<<a[e][f];
+// }
